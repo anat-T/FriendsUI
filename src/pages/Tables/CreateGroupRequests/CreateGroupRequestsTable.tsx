b94e827c1692e794e44422f0280daee559c89dfe
Anat <anat19960@gmail.com>
2022-01-23 15:37:19 +0200

    feat: add route for groupCreation and GroupManage

---
+++ src/pages/Tables/CreateGroupRequests/CreateGroupRequestsTable.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { makeStyles, Theme } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import DataTable from '../../../components/DataTable/DataTable';
+import * as ownerApi from '../../../utils/api-routes/owner.js';
+
+const useStyles = makeStyles((theme: Theme) => ({}));
+
+const rows = [
+    {
+        date: '02.03.2021',
+        friends: '14',
+        classify: 'סמצ',
+        groupType: 'תפוצת מייל',
+        nameOfRequester: 'hirrarchy',
+        group: '/מפקדת אסם/ענף חטיפים',
+        status: 'waiting',
+    },
+    {
+        date: '09.12.2020',
+        friends: '10',
+        classify: 'סמצ',
+        groupType: 'קבוצת אבטחה',
+        nameOfRequester: 'hirrarchy',
+        group: '/מפקדת אסם/ענף חטיפים',
+        status: 'approved',
+    },
+];
+
+const headers = ['תאריך ', 'חברים', 'סיווג', 'סוג קבוצה', 'שם מבקש', 'קבוצה', ''];
+
+export default function CreateGroupRequestsTable() {
+    const classes = useStyles();
+
+    // const [rows, setRows] = useState([] as any);
+
+    // const getGroups = async () => {
+    //     const newGroups = ownerApi.getOwnerRequestByCreator();
+    //     setRows(newGroups);
+    // };
+
+    // useEffect(() => {
+    //     getGroups();
+    // }, []);
+
+    return <DataTable rows={rows} headers={headers} type="approveAndDecline" title="יצירת קבוצה" />;
+}
